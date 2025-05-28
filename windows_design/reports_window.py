947e9aad857121ca4a7503622324ace84ac3d750
ilya3052 <ilya3052@ipopadin.ru>
2025-05-28 14:04:31 +0300

    ref: файлы дизайна

--- windows_design/reports_window.py
+++ windows_design/reports_window.py
@@ -16,8 +16,10 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenu, QMenuBar,
-    QSizePolicy, QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QGroupBox,
+    QHeaderView, QLabel, QMainWindow, QMenu,
+    QMenuBar, QPushButton, QSizePolicy, QSpacerItem,
+    QStatusBar, QTableWidget, QTableWidgetItem, QWidget)
 
 class Ui_reports(object):
     def setupUi(self, reports):
@@ -32,25 +34,77 @@ class Ui_reports(object):
         self.action_4.setObjectName(u"action_4")
         self.action_5 = QAction(reports)
         self.action_5.setObjectName(u"action_5")
+        self.problematic_couriers = QAction(reports)
+        self.problematic_couriers.setObjectName(u"problematic_couriers")
+        self.report_by_courier = QAction(reports)
+        self.report_by_courier.setObjectName(u"report_by_courier")
+        self.full_analytical_report = QAction(reports)
+        self.full_analytical_report.setObjectName(u"full_analytical_report")
         self.centralwidget = QWidget(reports)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.groupBox_2 = QGroupBox(self.centralwidget)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        self.groupBox_2.setStyleSheet(u"QGroupBox {border: none;}")
+        self.gridLayout_2 = QGridLayout(self.groupBox_2)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.label = QLabel(self.groupBox_2)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 1)
+
+        self.generate_report = QPushButton(self.groupBox_2)
+        self.generate_report.setObjectName(u"generate_report")
+        self.generate_report.setEnabled(True)
+
+        self.gridLayout_2.addWidget(self.generate_report, 2, 0, 1, 1)
+
+        self.courier_listbox = QComboBox(self.groupBox_2)
+        self.courier_listbox.setObjectName(u"courier_listbox")
+
+        self.gridLayout_2.addWidget(self.courier_listbox, 1, 0, 1, 1)
+
+
+        self.gridLayout.addWidget(self.groupBox_2, 1, 0, 1, 1)
+
+        self.reports_fields = QTableWidget(self.centralwidget)
+        self.reports_fields.setObjectName(u"reports_fields")
+
+        self.gridLayout.addWidget(self.reports_fields, 0, 0, 1, 3)
+
+        self.save_as_PDF = QPushButton(self.centralwidget)
+        self.save_as_PDF.setObjectName(u"save_as_PDF")
+
+        self.gridLayout.addWidget(self.save_as_PDF, 1, 2, 1, 1)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.gridLayout.addItem(self.horizontalSpacer, 1, 1, 1, 1)
+
         reports.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(reports)
         self.menubar.setObjectName(u"menubar")
         self.menubar.setGeometry(QRect(0, 0, 800, 22))
         self.menu = QMenu(self.menubar)
         self.menu.setObjectName(u"menu")
+        self.menu_2 = QMenu(self.menubar)
+        self.menu_2.setObjectName(u"menu_2")
         reports.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(reports)
         self.statusbar.setObjectName(u"statusbar")
         reports.setStatusBar(self.statusbar)
 
         self.menubar.addAction(self.menu.menuAction())
+        self.menubar.addAction(self.menu_2.menuAction())
         self.menu.addAction(self.summary)
         self.menu.addAction(self.action_2)
         self.menu.addAction(self.action_5)
         self.menu.addSeparator()
         self.menu.addAction(self.action_4)
+        self.menu_2.addAction(self.problematic_couriers)
+        self.menu_2.addAction(self.report_by_courier)
+        self.menu_2.addAction(self.full_analytical_report)
 
         self.retranslateUi(reports)
 
@@ -75,6 +129,23 @@ class Ui_reports(object):
 #if QT_CONFIG(shortcut)
         self.action_5.setShortcut(QCoreApplication.translate("reports", u"Ctrl+3", None))
 #endif // QT_CONFIG(shortcut)
+        self.problematic_couriers.setText(QCoreApplication.translate("reports", u"\u041f\u0440\u043e\u0431\u043b\u0435\u043c\u043d\u044b\u0435 \u043a\u0443\u0440\u044c\u0435\u0440\u044b", None))
+#if QT_CONFIG(shortcut)
+        self.problematic_couriers.setShortcut(QCoreApplication.translate("reports", u"Alt+1", None))
+#endif // QT_CONFIG(shortcut)
+        self.report_by_courier.setText(QCoreApplication.translate("reports", u"\u041e\u0442\u0447\u0435\u0442 \u043f\u043e \u043a\u0443\u0440\u044c\u0435\u0440\u0443", None))
+#if QT_CONFIG(shortcut)
+        self.report_by_courier.setShortcut(QCoreApplication.translate("reports", u"Alt+2", None))
+#endif // QT_CONFIG(shortcut)
+        self.full_analytical_report.setText(QCoreApplication.translate("reports", u"\u041e\u0442\u0447\u0435\u0442 \u043f\u043e \u0432\u0441\u0435\u043c \u043a\u0443\u0440\u044c\u0435\u0440\u0430\u043c", None))
+#if QT_CONFIG(shortcut)
+        self.full_analytical_report.setShortcut(QCoreApplication.translate("reports", u"Alt+3", None))
+#endif // QT_CONFIG(shortcut)
+        self.groupBox_2.setTitle("")
+        self.label.setText(QCoreApplication.translate("reports", u"\u0412\u044b\u0431\u0435\u0440\u0438\u0442\u0435 \u043a\u0443\u0440\u044c\u0435\u0440\u0430 \u0434\u043b\u044f \u0441\u043e\u0437\u0434\u0430\u043d\u0438\u044f \u043e\u0442\u0447\u0435\u0442\u0430", None))
+        self.generate_report.setText(QCoreApplication.translate("reports", u"\u0421\u0433\u0435\u043d\u0435\u0440\u0438\u0440\u043e\u0432\u0430\u0442\u044c \u043e\u0442\u0447\u0435\u0442", None))
+        self.save_as_PDF.setText(QCoreApplication.translate("reports", u"\u0421\u043e\u0445\u0440\u0430\u043d\u0438\u0442\u044c \u0432 PDF", None))
         self.menu.setTitle(QCoreApplication.translate("reports", u"\u041c\u0435\u043d\u044e", None))
+        self.menu_2.setTitle(QCoreApplication.translate("reports", u"\u041e\u0442\u0447\u0435\u0442\u044b", None))
     # retranslateUi
 
--- windows_design/summary_window.py
+++ windows_design/summary_window.py
@@ -16,14 +16,16 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenu, QMenuBar,
-    QSizePolicy, QStatusBar, QTextBrowser, QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QGroupBox, QHeaderView,
+    QLabel, QMainWindow, QMenu, QMenuBar,
+    QSizePolicy, QStatusBar, QTableWidget, QTableWidgetItem,
+    QVBoxLayout, QWidget)
 
 class Ui_summary(object):
     def setupUi(self, summary):
         if not summary.objectName():
             summary.setObjectName(u"summary")
-        summary.resize(800, 600)
+        summary.resize(797, 702)
         self.action_1 = QAction(summary)
         self.action_1.setObjectName(u"action_1")
         self.action_2 = QAction(summary)
@@ -39,13 +41,54 @@ class Ui_summary(object):
         self.reports.setObjectName(u"reports")
         self.centralwidget = QWidget(summary)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.textBrowser = QTextBrowser(self.centralwidget)
-        self.textBrowser.setObjectName(u"textBrowser")
-        self.textBrowser.setGeometry(QRect(20, 10, 671, 171))
+        self.groupBox = QGroupBox(self.centralwidget)
+        self.groupBox.setObjectName(u"groupBox")
+        self.groupBox.setGeometry(QRect(30, 0, 751, 381))
+        self.groupBox.setStyleSheet(u"QGroupBox {border: none;}")
+        self.gridLayout = QGridLayout(self.groupBox)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.couriers_summary = QLabel(self.groupBox)
+        self.couriers_summary.setObjectName(u"couriers_summary")
+        font = QFont()
+        font.setPointSize(11)
+        self.couriers_summary.setFont(font)
+
+        self.gridLayout.addWidget(self.couriers_summary, 2, 0, 1, 1)
+
+        self.delivery_summary = QLabel(self.groupBox)
+        self.delivery_summary.setObjectName(u"delivery_summary")
+        self.delivery_summary.setFont(font)
+
+        self.gridLayout.addWidget(self.delivery_summary, 1, 0, 1, 1)
+
+        self.orders_summary = QLabel(self.groupBox)
+        self.orders_summary.setObjectName(u"orders_summary")
+        self.orders_summary.setFont(font)
+
+        self.gridLayout.addWidget(self.orders_summary, 0, 0, 1, 1)
+
+        self.groupBox_2 = QGroupBox(self.centralwidget)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        self.groupBox_2.setGeometry(QRect(30, 390, 751, 250))
+        self.groupBox_2.setStyleSheet(u"QGroupBox {border: none;}")
+        self.verticalLayout = QVBoxLayout(self.groupBox_2)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.problematic_courier_label = QLabel(self.groupBox_2)
+        self.problematic_courier_label.setObjectName(u"problematic_courier_label")
+        self.problematic_courier_label.setFont(font)
+
+        self.verticalLayout.addWidget(self.problematic_courier_label)
+
+        self.problematic_couriers_summary = QTableWidget(self.groupBox_2)
+        self.problematic_couriers_summary.setObjectName(u"problematic_couriers_summary")
+        self.problematic_couriers_summary.setFont(font)
+
+        self.verticalLayout.addWidget(self.problematic_couriers_summary)
+
         summary.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(summary)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 22))
+        self.menubar.setGeometry(QRect(0, 0, 797, 22))
         self.menu = QMenu(self.menubar)
         self.menu.setObjectName(u"menu")
         self.menu_2 = QMenu(self.menubar)
@@ -88,23 +131,12 @@ class Ui_summary(object):
 #if QT_CONFIG(shortcut)
         self.reports.setShortcut(QCoreApplication.translate("summary", u"Ctrl+1", None))
 #endif // QT_CONFIG(shortcut)
-        self.textBrowser.setHtml(QCoreApplication.translate("summary", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"hr { height: 1px; border-width: 0; }\n"
-"li.unchecked::marker { content: \"\\2610\"; }\n"
-"li.checked::marker { content: \"\\2612\"; }\n"
-"</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">\u0417\u0430\u043f\u0438\u0445\u043d\u0443\u0442\u044c \u0441\u044e\u0434\u0430 \u043a\u0430\u043a\u0443\u044e-\u043d\u0438\u0431\u0443\u0434\u044c \u0441\u0442\u0430\u0442\u0438\u0441\u0442\u0438\u043a\u0443 \u043f\u043e \u0442\u0438\u043f\u0443 </p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">    \u041e\u0431\u0449\u0435"
-                        "\u0435 \u043a\u043e\u043b\u0438\u0447\u0435\u0441\u0442\u0432\u043e \u0437\u0430\u043a\u0430\u0437\u043e\u0432, \u043e\u0431\u0449\u0430\u044f \u0441\u0443\u043c\u043c\u0430 \u043a\u0443\u043f\u043b\u0435\u043d\u043d\u044b\u0445 \u0442\u043e\u0432\u0430\u0440\u043e\u0432, \u0441\u0430\u043c\u0430\u044f \u043f\u043e\u043f\u0443\u043b\u044f\u0440\u043d\u0430\u044f \u043a\u0430\u0442\u0435\u0433\u043e\u0440\u0438\u044f</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">    \u041a\u043e\u043b\u0438\u0447\u0435\u0441\u0442\u0432\u043e \u0437\u0430\u043a\u0430\u0437\u043e\u0432 \u0441 \u043e\u0446\u0435\u043d\u043a\u043e\u0439 5 \u0437\u0432\u0435\u0437\u0434 (+ \u043f\u0440\u043e\u0446\u0435\u043d\u0442\u043d\u043e\u0435 \u0441\u043e\u043e\u0442\u043d\u043e\u0448\u0435\u043d\u0438\u0435)</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">    4 \u0437\u0432\u0435"
-                        "\u0437\u0434\u044b</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">    3 \u0437\u0432\u0435\u0437\u0434\u044b \u0438 \u0442\u0434</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">    \u0422\u0440\u0435\u0431\u0443\u044e\u0449\u0438\u0435 \u0432\u043d\u0438\u043c\u0430\u043d\u0438\u044f \u043a\u0443\u0440\u044c\u0435\u0440\u044b (\u0443 \u043a\u043e\u0442\u043e\u0440\u044b\u0445 \u0437\u0430\u0431\u043b\u043e\u043a\u0438\u0440\u043e\u0432\u0430\u043d\u0430 \u0432\u043e\u0437\u043c\u043e\u0436\u043d\u043e\u0441\u0442\u044c \u043f\u0440\u0438\u043d\u0438\u043c\u0430\u0442\u044c \u0437\u0430\u043a\u0430\u0437\u044b)</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">\u0418\u043d\u0444\u0430 \u043e\u0431\u043d\u043e\u0432\u043b\u044f\u0435\u0442\u0441\u044f \u0434\u0438\u043d\u0430"
-                        "\u043c\u0438\u0447\u0435\u0441\u043a\u0438 \u043f\u043e\u0441\u0440\u0435\u0434\u0441\u0442\u0432\u043e\u043c \u0443\u0432\u0435\u0434\u043e\u043c\u043b\u0435\u043d\u0438\u0439 \u043e\u0442 \u0431\u0430\u0437\u044b, \u0432\u043e\u0437\u043c\u043e\u0436\u043d\u043e \u0447\u0430\u0441\u0442\u0438\u0447\u043d\u043e\u0435 \u043e\u0431\u043d\u043e\u0432\u043b\u0435\u043d\u0438\u0435 \u0442\u043e\u043b\u044c\u043a\u043e \u0438\u0437\u043c\u0435\u043d\u0438\u0432\u0448\u0435\u0439\u0441\u044f \u0438\u043d\u0444\u043e\u0440\u043c\u0430\u0446\u0438\u0438 (\u043d\u0430\u043f\u0440\u0438\u043c\u0435\u0440 \u043d\u0435\u0442 \u043d\u0443\u0436\u0434\u044b \u0438\u0441\u043a\u0430\u0442\u044c \u0437\u0430\u043d\u043e\u0432\u043e \u043f\u0440\u043e\u0431\u043b\u0435\u043c\u043d\u044b\u0445 \u043a\u0443\u0440\u044c\u0435\u0440\u043e\u0432 \u0435\u0441\u043b\u0438 \u0441\u0438\u0433\u043d\u0430\u043b \u043e\u0442 \u0431\u0430\u0437\u044b \u043d\u0435 \u043f\u043e\u0441\u0442\u0443\u043f\u0438\u043b)</p></body></html>", None))
+        self.groupBox.setTitle("")
+        self.couriers_summary.setText("")
+        self.delivery_summary.setText("")
+        self.orders_summary.setText("")
+        self.groupBox_2.setTitle("")
+        self.problematic_courier_label.setText(QCoreApplication.translate("summary", u"\u041a\u0443\u0440\u044c\u0435\u0440\u044b, \u043d\u0430 \u043a\u043e\u0442\u043e\u0440\u044b\u0445 \u0442\u0440\u0435\u0431\u0443\u0435\u0442\u0441\u044f \u043e\u0431\u0440\u0430\u0442\u0438\u0442\u044c \u0432\u043d\u0438\u043c\u0430\u043d\u0438\u0435", None))
         self.menu.setTitle(QCoreApplication.translate("summary", u"\u041c\u0435\u043d\u044e", None))
         self.menu_2.setTitle(QCoreApplication.translate("summary", u"\u041f\u043e\u043c\u043e\u0449\u044c", None))
     # retranslateUi
