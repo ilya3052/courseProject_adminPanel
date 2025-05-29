87e237f9281fea0a38dda2f3d15a589a0d9e6874
ilya3052 <ilya3052@ipopadin.ru>
2025-05-29 17:01:27 +0300

    add: открытие третьего окна с данными

--- windows/manager.py
+++ windows/manager.py
@@ -2,14 +2,23 @@ class WindowManager:
     def __init__(self):
         from windows.reports import Reports
         from windows.summary import SummaryInfo
+        from windows.data import Data
 
         self.reports_window = Reports(manager=self)
         self.summary_window = SummaryInfo(manager=self)
+        self.data_window = Data(manager=self)
 
     def show_reports(self):
         self.summary_window.hide()
+        self.data_window.hide()
         self.reports_window.show()
 
     def show_summary(self):
         self.reports_window.hide()
-        self.summary_window.show()
+        self.data_window.hide()
+        self.summary_window.showMaximized()
+
+    def show_data(self):
+        self.reports_window.hide()
+        self.summary_window.hide()
+        self.data_window.show()
