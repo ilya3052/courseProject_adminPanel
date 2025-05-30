e3413dc50f66515b457a84e847af9a80a7b8e76d
ilya3052 <ilya3052@ipopadin.ru>
2025-05-30 18:24:00 +0300

    ref: настройка асинхронного вызова для подписки на уведомления

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import sys
 
+import asyncio
+from PySide6.QtCore import QTimer
 from PySide6.QtWidgets import QApplication
+from qasync import QEventLoop
 
-from core import setup_logger
+from core import setup_logger, Database
 from windows import WindowManager
+
 
 if __name__ == "__main__":
     setup_logger()
     app = QApplication(sys.argv)
+
+    loop = QEventLoop(app)
+    asyncio.set_event_loop(loop)
+
     window = WindowManager()
     window.show_summary()
-    sys.exit(app.exec())
+
+    # Запускаем асинхронную задачу после старта event loop
+    def start_listening():
+        asyncio.create_task(
+            Database.listen_channel("courier_is_registered", window.data_window.stop_timer)
+        )
+
+    QTimer.singleShot(0, start_listening)
+
+    with loop:
+        loop.run_forever()
