f7b072f776da768ddcc8b9a1b5fb6215176a66ca
ilya3052 <ilya3052@ipopadin.ru>
2025-05-31 16:32:49 +0300

    add: класс для окна добавления продукта и часть функций

---
+++ windows/addProduct.py
@@ -0,0 +1,29 @@
+from PySide6.QtWidgets import QDialog
+
+from core import Database
+from windows_design import AddDataWindow
+
+
+class AddProduct(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self._ui = AddDataWindow()
+        self._ui.setupUi(self)
+
+        self.connect = Database.get_connection()
+
+        self.setup_actions()
+        self.fill_preset_field()
+
+    def setup_actions(self):
+        pass
+
+    def fill_preset_field(self):
+        self.set_article()
+
+    def set_article(self):
+        pass
+
+    def get_article(self) -> int:
+        pass
