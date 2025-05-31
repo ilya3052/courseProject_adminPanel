19b5ed7936c654a12d6ff169a57f0c3e7e40a77a
ilya3052 <ilya3052@ipopadin.ru>
2025-05-31 16:43:26 +0300

    add: сигнатуры основных функций

--- windows/addProduct.py
+++ windows/addProduct.py
@@ -13,6 +13,8 @@ class AddProduct(QDialog):
 
         self.connect = Database.get_connection()
 
+        self._ui.category_input.hide()
+
         self.article = None
         self.name = None
         self.category = None
@@ -24,10 +26,26 @@ class AddProduct(QDialog):
         self.fill_preset_field()
 
     def setup_actions(self):
-        pass
+        self._ui.name_input.textChanged.connect(self.name_changed)
+
+        self._ui.category_input.textChanged.connect(self.new_category_input)
+        self._ui.category_combobox.currentTextChanged.connect(self.category_changed)
+
+        self._ui.existsing_category.clicked.connect(self.existing_category)
+        self._ui.new_category.clicked.connect(self.new_category)
+
+        self._ui.price_input.valueChanged.connect(self.price_changed)
+
+        self._ui.description_input.textChanged.connect(self.description_changed)
+
+        self._ui.load_img.clicked.connect(self.load_img)
+
+        self._ui.save.clicked.connect(self.accept)
+        self._ui.cancel.clicked.connect(self.reject)
+
 
     def fill_preset_field(self):
-        self.set_article()
+        pass
 
     def set_article(self):
         pass
@@ -35,4 +53,26 @@ class AddProduct(QDialog):
     def get_article(self) -> int:
         pass
 
+    def name_changed(self):
+        pass
 
+    def category_changed(self):
+        pass
+
+    def new_category_input(self):
+        pass
+
+    def price_changed(self):
+        pass
+
+    def description_changed(self):
+        pass
+
+    def load_img(self):
+        pass
+
+    def existing_category(self):
+        pass
+
+    def new_category(self):
+        pass
